147b839d7734bacd7e687682e28f979019293265
Peter Wilson <peterwilsoncc@git.wordpress.org>
2026-05-06 05:19:40 +0000

    Admin Reskin: Correct alignment for spinners across the admin.
    Follow-up to [61646].
    
    Reviewed by peterwilsoncc.
    Merges r62291 to the 7.0 branch.
    
    Props wildworks, anupkankale, shailu25, gaurangsondagar, jadavsanjay, SergeyBiryukov.
    Fixes #65131.
    
    
    
    Built from https://develop.svn.wordpress.org/branches/7.0@62303
    
    
    git-svn-id: https://core.svn.wordpress.org/branches/7.0@61583 1a063a9b-81f0-0310-95a4-ce76da25c4cd

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -16,7 +16,7 @@
  *
  * @global string $wp_version
  */
-$wp_version = '7.0-RC2-62302';
+$wp_version = '7.0-RC2-62303';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
